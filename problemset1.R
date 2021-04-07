12164375f5b9400834b745047d1de246602d8767
jonschth <jst@arev.is>
2021-04-07 17:17:47 +0000

    enn eitt committeð

--- problemset1.R
+++ problemset1.R
@@ -5,3 +5,45 @@ library (dslabs)
 library(rmarkdown)
 
 view(polls_us_election_2016)
+
+# Graph 1
+ggplot(data = polls_us_election_2016) +
+  geom_point(mapping = aes(x = startdate,
+                           y = samplesize,
+                           color = rawpoll_johnson))
+
+# Graph 2
+ggplot(data = polls_us_election_2016) +
+  geom_point(mapping = aes(x = startdate,
+                           y = samplesize,
+                           color = as.character(rawpoll_johnson)))
+
+ggplot(data = polls_us_election_2016) +
+  geom_point(mapping = aes(x = startdate,
+                           y = rawpoll_clinton,
+                           color = samplesize >= 500))
+
+ggplot(data = polls_us_election_2016) +
+  geom_point(mapping = aes(x = startdate,
+                           y = rawpoll_clinton),
+                           colour= "blue")
+
+
+#2.3
+ggplot(data = polls_us_election_2016) +
+  geom_point(mapping = aes(x = startdate,
+                           y = rawpoll_clinton)) +
+  facet_grid(cols = vars(population))
+
+ggplot(data = polls_us_election_2016) +
+  geom_point(mapping = aes(x = startdate,
+                           y = rawpoll_clinton)) +
+  facet_grid(cols = vars(grade))
+
+
+ggplot(data = polls_us_election_2016) +
+  geom_point(mapping = aes(x = startdate,
+                           y = rawpoll_clinton)) +
+  facet_grid(rows = vars(grade), cols = vars(population))
+
+
