c193f088c69609a8806ded2308b153dcf2425140
jonschth <jst@arev.is>
2021-04-07 18:27:30 +0000

    qtpq

--- problemset1.R
+++ problemset1.R
@@ -47,3 +47,50 @@ ggplot(data = polls_us_election_2016) +
   facet_grid(rows = vars(grade), cols = vars(population))
 
 
+
+ggplot(data = polls_us_election_2016) +
+  geom_point(mapping = aes(x = startdate,
+                           y = rawpoll_clinton)) +
+  facet_wrap(~rawpoll_clinton+align,ncol=4)
+
+
+
+
+
+
+DF <- data.frame(WindDir=sample(0:180, 20, replace=T), 
+                 WindSpeed=sample(1:40, 20, replace=T), 
+                 Force=sample(1:40, 20, replace=T))
+
+
+qplot(WindSpeed, Force,
+      data = transform(DF,
+                       fct = cut(WindDir, seq(0,180,3))),
+      facets=~fct)
+
+
+
+ggplot(data = polls_us_election_2016) +
+  geom_point(mapping = aes(x = startdate,
+                           y = rawpoll_trump,
+                           color = grade))+
+  facet_grid(rows=vars(population))
+
+
+
+ggplot(data = polls_us_election_2016,
+       mapping = aes(x = startdate,
+                     y = rawpoll_trump)) +
+  geom_point() +
+  geom_smooth(se=TRUE)
+
+
+ggplot() +
+  geom_point(data = polls_us_election_2016,
+             mapping = aes(x = startdate,
+                           y = rawpoll_trump)) +
+  geom_smooth(data = polls_us_election_2016,
+              mapping = aes(x = startdate,
+                            y = rawpoll_trump), se=FALSE)
+
+
